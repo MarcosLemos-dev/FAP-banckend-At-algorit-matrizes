ddc90df8930eebd0a05c3318dc6c40bf8fdb9ff5
MarcosLemos-dev <98950878+MarcosLemos-dev@users.noreply.github.com>
2023-09-16 17:29:48 -0300

    subindo algoritmo sobre matriz

---
+++ matrizes.js
@@ -0,0 +1,17 @@
+/*Instruções
+Analise a tabela de dados em anexo e crie as estruturas necessárias em JavaScript
+(vetores ou matrizes) para representar os dados presentes nela.
+Realize essa atividade no WORD ou no Bloco de Notas, suba esse arquivo para
+algum repositório e compartilhe o link no campo ao lado para que outros
+desenvolvedores possam analisá-lo.
+ */
+
+let matriz = [ 
+    ['José',19,1900,true], 
+    ['David',22,2500,false],
+    ['Maria',25,3400,true],
+    ['Igor',18,2100,false]
+];
+
+console.log('|Nome|idade|salario|tem ensino medio completo?|',matriz);
+
